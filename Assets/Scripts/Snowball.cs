f14e9860ac118c83a8c33763fb725d35501cb68e
Richard <richrdhw@gmail.com>
2019-10-29 12:18:53 -0400

    Update Snowball.cs

--- Assets/Scripts/Snowball.cs
+++ Assets/Scripts/Snowball.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class Snowball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public MoveTo MoveTo_Snowball;
+    public Move Move_Player;
+    
+    public float size;
+
+    public Transform snowball;
+    public Transform snowballWaypoint;
+    public Transform TargetSnowball;
+    //Snowball stacking
+    public float stackOffset;
+    public bool stacking;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
+    }
+
+
+
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        if (Move_Player = null)
+    //        {
+    //            Move_Player = other.gameObject.GetComponent<Move>();
+
+    //        }
+    //    }
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Move_Player = null)
+            {
+                Move_Player = collision.gameObject.GetComponent<Move>();
+
+            }
+        }
+
+    }
+
+
+        private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Move_Player != null)
+            {
+                this.transform.position += Move_Player.newDir * Time.deltaTime * Move_Player.moveSpeed;
+            }
+        }
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Move_Player != null)
+            {
+                Move_Player = null;
+            }
+        }
+
     }
 }
